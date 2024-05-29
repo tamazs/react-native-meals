fb0521468c982e44651ebc11d9f6306746a03d40
Tamás Major <majortamas313@gmail.com>
2024-05-29 13:48:47 +0200

    passing category ID

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -8,7 +8,9 @@ import CategoryGridTile from "../components/CategoryGridTile";
 function CategoriesScreen({ navigation }) {
     function renderCategoryItem(itemData) {
         function pressHandler() {
-            navigation.navigate('MealsOverview')
+            navigation.navigate('MealsOverview', {
+                categoryId: itemData.item.id
+            })
         }
 
         return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onPress={pressHandler} />;
--- screens/MealsOverViewScreen.js
+++ screens/MealsOverViewScreen.js
@@ -2,10 +2,12 @@ import { View, Text, StyleSheet } from "react-native";
 
 import { MEALS } from "../data/dummy-data";
 
-function MealsOverViewScreen() {
+function MealsOverViewScreen({ route }) {
+    const catId = route.params.categoryId
+
     return (
         <View style={styles.container}>
-            <Text>MealsOverViewScreen</Text>
+            <Text>MealsOverViewScreen - {catId}</Text>
         </View>
     )
 }
@@ -14,7 +16,7 @@ export default MealsOverViewScreen;
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
+        flex: 1,
         padding: 16
     }
 })
