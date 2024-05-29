207bce0579151265040313b5da9cac2523b22d94
Tamás Major <majortamas313@gmail.com>
2024-05-29 14:01:04 +0200

    displaying meal items

--- screens/MealsOverViewScreen.js
+++ screens/MealsOverViewScreen.js
@@ -1,13 +1,22 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
 
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 function MealsOverViewScreen({ route }) {
     const catId = route.params.categoryId
 
+    const displayedMeals = MEALS.filter((meal) =>
+        meal.categoryIds.includes(catId)
+    );
+
+    function renderMealItem(itemData) {
+        return <MealItem title={itemData.item.title}/>
+    }
+
     return (
         <View style={styles.container}>
-            <Text>MealsOverViewScreen - {catId}</Text>
+            <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem}/>
         </View>
     )
 }
