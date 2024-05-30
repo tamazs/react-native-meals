63e039e6f833dfd3083b8d5e6ff7cd822f8c86e7
Tamás Major <majortamas313@gmail.com>
2024-05-30 09:22:10 +0200

    header styling

--- screens/MealsOverViewScreen.js
+++ screens/MealsOverViewScreen.js
@@ -1,14 +1,23 @@
 import { View, StyleSheet, FlatList } from "react-native";
+import { useLayoutEffect } from "react";
 
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
 
-function MealsOverViewScreen({ route }) {
+function MealsOverViewScreen({ route, navigation }) {
     const catId = route.params.categoryId
 
     const displayedMeals = MEALS.filter((meal) =>
         meal.categoryIds.includes(catId)
     );
+
+    useLayoutEffect(() => {
+        const catTitle = CATEGORIES.find((category) => category.id === catId).title;
+
+        navigation.setOptions({
+            title: catTitle
+        })
+    }, [catId, navigation])
 
     function renderMealItem(itemData) {
         const item = itemData.item;
@@ -20,12 +29,12 @@ function MealsOverViewScreen({ route }) {
             complexity: item.complexity,
             duration: item.duration
         }
-        return <MealItem {...mealItemProps}/>
+        return <MealItem {...mealItemProps} />
     }
 
     return (
         <View style={styles.container}>
-            <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem}/>
+            <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem} />
         </View>
     )
 }
